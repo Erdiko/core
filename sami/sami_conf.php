1f652507e62530349195da795bd03b47704ae99e
colemantung <cmtkusc@gmail.com>
2014-07-29 12:02:45 -0700

    Updated Sami’s config.

--- sami/sami_conf.php
+++ sami/sami_conf.php
@@ -12,20 +12,20 @@ $iterator = Finder::create()
     ->exclude('tests')
     ->in($dir)
 ;
-/*
+
 $versions = GitVersionCollection::create($dir)
     //->addFromTags('v1.0.*')
     //->add('1.0', '1.0 branch')
     ->add('master', 'master branch')
 ;
-*/
+
 return new Sami($iterator, array(
     'theme'                => 'symfony',
-    //'versions'             => $versions,
-    'versions'             => '0.2',
+    'versions'             => $versions,
+    //'versions'             => '0.2',
     'title'                => 'Erdiko API',
-    'build_dir'            => __DIR__.'/build/',
-    'cache_dir'            => __DIR__.'/cache/',
+    'build_dir'            => __DIR__.'/build/%version%/',
+    'cache_dir'            => __DIR__.'/cache/%version%/',
     //  'simulate_namespaces'  => true, 
     'default_opened_level' => 1,
 ));
