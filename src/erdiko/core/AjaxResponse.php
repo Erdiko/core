3d941852251a61410978f296520520be18b099f8
Andy Armstrong <andyarmstro@gmail.com>
2015-04-28 22:14:40 -0700

    Set the HTTP status code in AJAX responses
    Actually return the HTTP status with the header response, and include it in the AJAX response for debugging.

--- src/erdiko/core/AjaxResponse.php
+++ src/erdiko/core/AjaxResponse.php
@@ -5,7 +5,9 @@
  * @category   Erdiko
  * @package    Core
  * @copyright  Copyright (c) 2014, Arroyo Labs, http://www.arroyolabs.com
- * @author	   John Arroyo
+ * 
+ * @author	   John Arroyo john@arroyolabs.com
+ * @author	   Andy Armstrong andy@arroyolabs.com
  */
 namespace erdiko\core;
 use Erdiko;
@@ -15,14 +17,36 @@ use Erdiko;
  */
 class AjaxResponse extends Response 
 {
+
+    /**
+     * _statusCode
+     *
+     * Unless explicitly set, default to a 200 status 
+     * assuming everything went ok.
+     */
+    protected $_statusCode = 200;
+
     /**
      * Theme
      */
-	protected $_theme;
+    protected $_theme;
+
     /**
      * Content
      */
-	protected $_content = null;
+    protected $_content = null;
+
+    /**
+     * setStatusCode
+     * 
+     * Set, and send, the HTTP status code. 
+     */
+    public function setStatusCode($code = null) {
+      if(!empty($code)) {
+        $this->_statusCode = $code;
+        http_response_code($this->_statusCode); // this sends the http status code
+      }
+    }
 
     /**
      * Ajax render function
@@ -31,13 +55,13 @@ class AjaxResponse extends Response
      */
     public function render()
     {
-        $responseData = array(
-            "status" => 500,
-            "body" => $this->_content,
-            "errors" => array()
-            );
+      $responseData = array(
+                              "status" => $this->_statusCode,
+                              "body" => $this->_content,
+                              "errors" => array()
+                            );
 
-        return json_encode($responseData);
+      return json_encode($responseData);
     }
 
 }
