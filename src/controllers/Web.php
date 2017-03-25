962fefb2e07fea8870972d2230746aaaf6311703
john arroyo <john.arroyo@gmail.com>
2017-03-25 00:52:54 -0700

    add auto action to web controller

--- src/controllers/Web.php
+++ src/controllers/Web.php
@@ -12,11 +12,25 @@ namespace erdiko\controllers;
 
 class Web extends \erdiko\Controller
 {
-    protected function determineAction($request, $args = null)
+    public function __invoke($request, $response, $args) 
+    {
+        $action = $this->determineAction($request, $args);
+        $this->container->logger->debug("Controller action: ".print_r($action, true));
+
+        $this->$action($request, $response, $args); 
+    }
+    
+    protected function determineAction(&$request, &$args) : string
     {
         // REQUEST_METHOD
         $action = strtolower($request->getMethod());
-        $this->container->logger->debug("action: {$action}");
-        $this->container->logger->debug("args: ".print_r($args, true));
+        // Action
+        $action .= ucfirst($args['action']);
+
+         // @todo trigger 404 instead of throw exception
+        if(!method_exists($this, $action))
+            throw new \Exception("action does not exist");
+
+        return $action;
     }
 }
